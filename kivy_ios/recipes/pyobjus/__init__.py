26a0babcc6f9764c449f55c6bece99a28dea47ee
prasadmadanayake <prasadmadanayake7@gmail.com>
2025-09-17 13:26:32 +0300

    Update __init__.py

--- kivy_ios/recipes/pyobjus/__init__.py
+++ kivy_ios/recipes/pyobjus/__init__.py
@@ -3,7 +3,8 @@ from kivy_ios.toolchain import CythonRecipe
 
 class PyobjusRecipe(CythonRecipe):
     version = "master"
-    url = "https://github.com/kivy/prasadmadanayake/archive/{version}.zip"
+    #url = "https://github.com/kivy/prasadmadanayake/archive/{version}.zip"
+    url = "https://github.com/prasadmadanayake/pyobjus/archive/{version}.zip"
     library = "libpyobjus.a"
     depends = ["python"]
     pre_build_ext = True
